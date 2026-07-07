3a1d2f9d427e46807763eb0aa54699f6568452f2
Shusei Yokoi <your-email@example.com>
2026-07-06 22:44:45 -0700

    add front-end

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 """URL configuration for the cast-it podcast builder project."""
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
